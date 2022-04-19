0796a257be394f3c17a9e8b9a65328f09fbe54c8
hslim <myohoon95@gmail.com>
2022-04-20 00:04:09 +0900

    애플, 네이버, 구글 로그인 작업 완료. 키설정 및 테스트 필요

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -3,11 +3,10 @@ import Flutter
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
-  override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
+  override func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+
+    // 네이버 로그인
+    return NaverThirdPartyLoginConnection.getSharedInstance().application(app, open: url, options: options)
   }
 }
