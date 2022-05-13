1ebf9ab9fadcee6624de8ec59c97abd28a7c0f18
ocko112 <ocko112@gmail.com>
2022-05-14 00:38:13 +0900

    상세보기 작업중

--- lib/view/page/mume/infinite_detail_page.dart
+++ lib/view/page/mume/infinite_detail_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:badges/badges.dart';
 import 'package:mume/model/dto/mume/infinite_detail.dart';
 
 class InfiniteDetailPage extends StatefulWidget {
@@ -20,23 +21,56 @@ class _InfiniteDetailState extends State<InfiniteDetailPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        //title: Text(widget.infiniteDetail.symbol!),
-        title: Text("widget.infiniteDetail.symbol!"),
-        centerTitle: true,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
         actions: [
           IconButton(
-            icon: Icon(Icons.edit),
+            icon: const Icon(Icons.edit),
             onPressed: () {
             },
           ),
           IconButton(
-              icon: Icon(Icons.remove),
+              icon: const Icon(Icons.delete),
               onPressed: () {
               },
           )
         ],
       ),
       body: Container(
+        //alignment: Alignment.center,
+        padding: const EdgeInsets.fromLTRB(25, 10, 25, 10),
+        child: Column(
+          children: [
+            Container(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(widget.infiniteDetail!.symbol!, style: TextStyle(fontSize: 20)),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Text("진행중"),
+                      Text("계좌"),
+                      Text("v.2.1"),
+                      Badge(
+                        toAnimate: false,
+                        shape: BadgeShape.square,
+                        badgeColor: Colors.lightBlue,
+                        padding: EdgeInsets.fromLTRB(10, 4, 10, 4),
+                        borderRadius: BorderRadius.circular(20),
+                        badgeContent: Text('작업중', style: TextStyle(color: Colors.white)),
+                      )
+                    ]
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
 
       )
     );
