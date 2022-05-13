455dc5c5287bd52c87e6955e0e080ded49c0c402
ocko112 <ocko112@gmail.com>
2022-05-14 01:27:49 +0900

    상세보기 작업중

--- lib/view/page/mume/infinite_detail_page.dart
+++ lib/view/page/mume/infinite_detail_page.dart
@@ -42,33 +42,104 @@ class _InfiniteDetailState extends State<InfiniteDetailPage> {
       ),
       body: Container(
         //alignment: Alignment.center,
-        padding: const EdgeInsets.fromLTRB(25, 10, 25, 10),
+        padding: const EdgeInsets.fromLTRB(25, 20, 25, 20),
         child: Column(
           children: [
             Container(
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Text(widget.infiniteDetail!.symbol!, style: TextStyle(fontSize: 20)),
+                  Text(widget.infiniteDetail!.symbol!, style: TextStyle(fontSize: 25, fontWeight: FontWeight.w600)),
                   Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
                     children: [
-                      Text("진행중"),
-                      Text("계좌"),
-                      Text("v.2.1"),
-                      Badge(
-                        toAnimate: false,
-                        shape: BadgeShape.square,
-                        badgeColor: Colors.lightBlue,
-                        padding: EdgeInsets.fromLTRB(10, 4, 10, 4),
-                        borderRadius: BorderRadius.circular(20),
-                        badgeContent: Text('작업중', style: TextStyle(color: Colors.white)),
+                      Container(
+                        child: Badge(
+                          toAnimate: false,
+                          shape: BadgeShape.square,
+                          badgeColor: (widget.infiniteDetail!.infiniteState! == "진행중" ? Colors.blue[700]! :
+                            widget.infiniteDetail!.infiniteState! == "매도완료" ? Colors.green[500]! :
+                            widget.infiniteDetail!.infiniteState! == "매도중지" ? Colors.orange : Colors.red
+                          ),
+                          padding: EdgeInsets.fromLTRB(10, 4, 10, 4),
+                          borderRadius: BorderRadius.circular(8),
+                          badgeContent: Text(widget.infiniteDetail!.infiniteState!, style: TextStyle(fontSize: 12, color: Colors.white)),
+                        )
                       )
                     ]
                   ),
+                  
+                  
                 ],
               ),
-            )
+            ),
+            Container(
+              alignment: Alignment.centerLeft,
+              padding: EdgeInsets.fromLTRB(0, 10, 0, 50),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text("진행률", style: TextStyle()),
+                  Text("0.0%", style: TextStyle())
+                ],
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
+              child: Column(
+                children: [
+                  Container(
+                    alignment: Alignment.centerLeft,
+                    padding: EdgeInsets.only(bottom: 5),
+                    child: Text("기본정보", style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600)),
+                  ),
+                  Container(
+                    padding: EdgeInsets.only(bottom: 5),
+                    child: Row(
+                      children: [
+                        Expanded(
+                          flex: 5,
+                          child: Text("시작일자", style: TextStyle(color: Colors.black54))
+                        ),
+                        Expanded(
+                          flex: 5,
+                          child: Text("평단가", style: TextStyle(color: Colors.black54))
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    padding: EdgeInsets.only(bottom: 10),
+                    child: Row(
+                      children: [
+                        Expanded(
+                          flex: 5,
+                          child: Text(widget.infiniteDetail!.startedDate!, style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600))
+                        ),
+                        Expanded(
+                          flex: 5,
+                          child: Text("\$" + widget.infiniteDetail!.averagePrice!.toString(), style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600))
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    padding: EdgeInsets.only(bottom: 5),
+                    child: Row(
+                      children: [
+                        Expanded(
+                          flex: 5,
+                          child: Text("시작일자", style: TextStyle(color: Colors.black54))
+                        ),
+                        Expanded(
+                          flex: 5,
+                          child: Text("평단가", style: TextStyle(color: Colors.black54))
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
 
